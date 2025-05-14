54edb55da2d8c3e0ec91e15f56326f3b8f005ab8
torhulugh <torhulugh@gmail.com>
2025-05-14 19:58:56 -0400

    week2 varaiable

---
+++ wee2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>week 2</title>
+</head>
+<body>
+    <h1>Hello  hello!</h1>
+    <?php
+    $fname = "Tor";
+    $lname = "Hulugh";
+    echo "Hello" . $fname;
+    ?>
+    
+</body>
+</html>
